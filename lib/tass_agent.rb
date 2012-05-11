e0a01ea4b71c507f6a32627318bc4f7b99061623
MillisamiSprout <millisami@gmail.com>
2012-05-11 18:39:43 +0545

    changed the flags for commands

--- lib/tass_agent.rb
+++ lib/tass_agent.rb
@@ -6,12 +6,16 @@ module TassAgent
   def run
     process_cmd_cpu = "ps -e -o etime,pid,ppid,command,rss,pcpu | grep [u]nicorn | awk '{ sum += $NF } END { print sum }'"
 
-    mem_cmd = "top | head -5 | grep -w Mem | awk -F, '{ print $2 }' | awk -Fused '{print $1}' | awk '{print $2}'"
+    mem_cmd = "top -b | head -5 | grep -w Mem | awk -F, '{ print $2 }' | awk -Fused '{print $1}' | awk '{print $1}'"
 
-    cpu_cmd = "top | head -5 | grep -w Cpu | awk -F, '{print $4 }' | awk -F% '{ print $1 }'"
+    cpu_cmd = "top -b | head -5 | grep -w Cpu | awk -F, '{print $4 }' | awk -F% '{ print $1 }'"
 
     puts system(mem_cmd)
     puts system(cpu_cmd)
     puts system(process_cmd_cpu)    
+    
+    # Results send to remote http service
+    
+    
   end
 end
