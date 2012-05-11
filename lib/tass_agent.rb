86a53d7e8be7b50828e636a22b1cf8219304d461
MillisamiSprout <millisami@gmail.com>
2012-05-11 13:10:11 +0545

    run method added

--- lib/tass_agent.rb
+++ lib/tass_agent.rb
@@ -1,5 +1,17 @@
 require "tass_agent/version"
 
 module TassAgent
-  # Your code goes here...
+  extend self
+  
+  def run
+    process_cmd_cpu = "ps -e -o etime,pid,ppid,command,rss,pcpu | grep [u]nicorn | awk '{ sum += $NF } END { print sum }'"
+
+    mem_cmd = "top | head -5 | grep -w Mem | awk -F, '{ print $2 }' | awk -Fused '{print $1}' | awk '{print $2}'"
+
+    cpu_cmd = "top | head -5 | grep -w Cpu | awk -F, '{print $4 }' | awk -F% '{ print $1 }'"
+
+    puts system(mem_cmd)
+    puts system(cpu_cmd)
+    puts system(process_cmd_cpu)    
+  end
 end
